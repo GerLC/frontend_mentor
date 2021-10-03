09410738ec8ac0218c6b2fef76ab693ca5f754f6
GerLC <gli.16@est.ucab.edu.ve>
2021-10-03 10:57:48 -0400

    [feat] url-shortening remove link - Add remove function to remove a link

--- url-shortening-api-master/app/js/script.js
+++ url-shortening-api-master/app/js/script.js
@@ -52,6 +52,9 @@ const appendDiv = (url, shortUrl) => {
     <p class="overflow-ellipsis" onclick='copyClip(event)'>
       ${url}
     </p>
+    <div class="icon trash" onclick='remove(event)'>
+      <svg id="ecUvfY3VaWr1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><g id="ecUvfY3VaWr2"><path id="ecUvfY3VaWr3" d="M424,64L336,64L336,48C336,21.49,314.51,0,288,0L224,0C197.49,0,176,21.49,176,48L176,64L88,64C65.909,64,48,81.909,48,104L48,136C48,144.837,55.163,152,64,152L448,152C456.837,152,464,144.837,464,136L464,104C464,81.909,446.091,64,424,64ZM208,48C208,39.18,215.18,32,224,32L288,32C296.82,32,304,39.18,304,48L304,64L208,64Z" transform="matrix(1 0 0 1 0.000004 32)" fill="rgb(0,0,0)" stroke="none" stroke-width="1"/><path id="ecUvfY3VaWr4" d="M78.364,184C75.509,184,73.234,186.386,73.37,189.238L86.57,466.28C87.79,491.92,108.85,512,134.51,512L377.49,512C403.15,512,424.21,491.92,425.43,466.28L438.63,189.238C438.766,186.386,436.491,184,433.636,184ZM320,224C320,215.16,327.16,208,336,208C344.84,208,352,215.16,352,224L352,432C352,440.84,344.84,448,336,448C327.16,448,320,440.84,320,432ZM240,224C240,215.16,247.16,208,256,208C264.84,208,272,215.16,272,224L272,432C272,440.84,264.84,448,256,448C247.16,448,240,440.84,240,432ZM160,224C160,215.16,167.16,208,176,208C184.84,208,192,215.16,192,224L192,432C192,440.84,184.84,448,176,448C167.16,448,160,440.84,160,432Z" fill="rgb(0,0,0)" stroke="none" stroke-width="1"/></g></svg>
+    </div>
   </div>
   <div class="card__divider"></div>
   <div class="card__content">
@@ -142,6 +145,22 @@ const successCopy = (e) => {
 }
 
 
+const removeArrayItem = (item) => {
+  for (let i=0; i<linksSaved.length; i++) {
+    console.log(linksSaved[i].trim() == item.trim())
+    console.log(linksSaved[i].trim())
+    console.log(item.trim())
+
+    if(linksSaved[i].trim() == item.trim()) {
+      linksSaved.splice(i, 1); 
+      break;
+    }
+
+  }
+
+}
+
+
 // Load Links in Storage on Refresh
 async function shortenLinkOnLoadStorage(url) {
   try {
@@ -165,7 +184,7 @@ async function getShortenLink(url) {
     const response = await fetch(`${urlApi}${url}`);
     const data = await response.json();
     let shortLink = data.result.short_link;
-    console.log(shortLink);
+    // console.log(shortLink);
     appendDiv(url, shortLink);
     linksSaved.push(url);
     success();
@@ -179,7 +198,7 @@ async function getShortenLink(url) {
 }
 
 const openBtn = () => {
-  console.log('click hamburger');
+  // console.log('click hamburger');
   header.classList.toggle('open');
   fadeElements();
 }
@@ -203,9 +222,22 @@ function copyClip(event) {
 }
 
 
+const remove = (event) => {
+  const element = event.currentTarget.closest('.card__result');
+  const link = event.currentTarget.parentElement.querySelector('.overflow-ellipsis');
+
+  removeArrayItem(link.textContent);
+  storeLink();
+  element.classList.add('removed');
+  
+  setTimeout(()=> {
+    element.remove();
+  }, 1000);
+}
+
 function getLinks() {
   linksSaved.forEach(x => {
-    console.log(x)
+    // console.log(x)
     shortenLinkOnLoadStorage(x);
   })
 }
