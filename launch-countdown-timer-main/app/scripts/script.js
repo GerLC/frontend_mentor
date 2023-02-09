33191e72f2003156cd6fc2704d1c16be9806f651
GerLC <gli.16@est.ucab.edu.ve>
2023-02-09 12:32:58 -0400

    fix background image

--- launch-countdown-timer-main/app/scripts/script.js
+++ launch-countdown-timer-main/app/scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let _timer = 1633905999;
+    let _timer = 1686341775;
     let flipdown = new FlipDown(_timer)
         .start()
         .ifEnded(() => {
